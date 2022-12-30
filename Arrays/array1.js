ad0a2729861039732d311b32f71cdd1baaf0020c
saif0110 <khanmohdtauheed4@gmail.com>
2022-12-30 15:25:46 +0530

    creating an array

---
+++ Arrays/array1.js
@@ -0,0 +1,18 @@
+/*
+    here we will learn how to make array and use there methods like:
+    1. pop();
+    1. unshift();
+    2. shift();
+    3. indexOf();
+*/
+
+var name = ["Saif", "Sama", "Tauheed", "Salman Bhai"];
+name.pop();
+console.log(name);
+// name.unshift("NEW ADDED");
+// console.log(name);
+// name.shift();
+// console.log(name);
+// name.shift();
+// console.log(name);
+
